828a37e74bd11e912341795b29d0f1ea8f15d6b2
Paulo Amorim <paulorec@gmail.com>
2018-03-04 15:03:17 -0300

    :sparkles: hello world

---
+++ index.js
@@ -0,0 +1,12 @@
+//Import a library to help create a component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+//Create a component
+
+const App = () => (
+        <Text>Some Text</Text>
+);
+
+//Render it to the device
+AppRegistry.registerComponent('albuns', () => App);
